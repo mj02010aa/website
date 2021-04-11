cde03c27e0f7d7a2b1ed3fe4c1a375c3839e9771
Eleven <elevenvac@gmail.com>
2021-04-11 13:41:22 +0300

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,7 @@
 <template>
      <img src="assets/logo.jpg" width="100">
      <h2>Website</h2>
-     <Foot />
 </template>
-
-<!--<script lang="ts">
-import { defineComponent } from "vue";
-import Foot from "./components/Foot.vue";
-export default defineComponent({
-  name: "App",
-  components: {
-    Foot
-  }
-});
-</script>-->
 
 <style>
 #app {
