487753d6d82cefe0a5c02e69e0448eb6b1a2eb95
Eleven <elevenvac@gmail.com>
2021-04-11 13:36:09 +0300

    Create Foot.vue

---
+++ src/components/Foot.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-footer padless>
+    <v-col
+      class="text-center"
+      cols="12"
+    >
+      <strong>{{ new Date().getFullYear() }} © <a href="//eleven.js.org" target="_blank">Eleven</a> — Made with &#x2764; using <a href="//vuejs.org" target="_blank">Vue</a>.</strong>
+    </v-col>
+  </v-footer>
+</template>
+
+<style>
+a:hover {
+  text-decoration: underline;
+}
+</style>
