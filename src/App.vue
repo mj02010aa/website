99f07a908ace814841aa2d03e94a8e3a51507601
Eleven <elevenvac@gmail.com>
2021-04-11 13:48:21 +0300

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -3,12 +3,15 @@
 </template>
 
 <style>
-#app {
+@import"https://fonts.googleapis.com/css2?family=Inter:wght@100;300;400;500;700;900&display=swap";
+body {
   display: flex;
   justify-content: center;
   align-items: center;
   flex-direction: column;
   width: 100%;
+  align-items: center;
+  font-family: "Inter";
 }
 h2 {
   margin-bottom: 0;
