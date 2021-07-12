cca98094c56cd13f2d3eff5831d612fc0bd43efc
Eleven <incealper00@gmail.com>
2021-07-13 00:56:38 +0300

    fix: development to sas

--- src/App.vue
+++ src/App.vue
@@ -17,7 +17,7 @@
       <section class="home">
          <h1 class="mt-normal">Hi, I'm Eleven<span class="dot-green">.</span></h1>
          <p class="mt-normal">
-            I'm a Student & Full-stack developer. I'm a Currently doing projects under name of <a class="hyperlink" href="https://github.com/risedevelopment" target="_blank">Rise Development</a>. If you are curious about the music I listen to while working, you can check out my <a class="hyperlink" href="https://open.spotify.com/user/yo55g26ffwx83q0smizx52yuf" target="_blank">Spotify</a> playlists. I also share all my open source projects on <a class="hyperlink" href="https://github.com/elevenvac" target="_blank">Github</a>, including this website.
+            I'm a Student & Full-stack developer. I'm a Currently doing projects under name of <a class="hyperlink" href="https://github.com/aventursas" target="_blank">Aventur SAS</a>. If you are curious about the music I listen to while working, you can check out my <a class="hyperlink" href="https://open.spotify.com/user/yo55g26ffwx83q0smizx52yuf" target="_blank">Spotify</a> playlists. I also share all my open source projects on <a class="hyperlink" href="https://github.com/elevenvac" target="_blank">Github</a>, including this website.
          </p>
          <a href="mailto:11@apptime.tech" target="_blank" class="semi hyperlink mt-normal">11@apptime.tech</a>
       </section>
