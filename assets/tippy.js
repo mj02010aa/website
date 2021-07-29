074ef633c4041131169e6348b2f3ef3f908bab84
Eleven <incealper00@gmail.com>
2021-07-29 17:31:35 +0300

    Create tippy.js

---
+++ assets/tippy.js
@@ -0,0 +1,48 @@
+  tippy('#goHomeImage', {
+    content: 'Go Home',
+    arrow: false,
+    animation: 'scale',
+    theme: 'ws',
+  });
+
+  tippy('#sociDiscord', {
+    content: 'View my Discord Profile',
+    arrow: false,
+    animation: 'scale',
+    theme: 'ws',
+  });
+
+  tippy('#sociGithub', {
+    content: 'View my GitHub Profile',
+    arrow: false,
+    animation: 'scale',
+    theme: 'ws',
+  });
+
+  tippy('#sociSpotify', {
+    content: 'View my Spotify Profile',
+    arrow: false,
+    animation: 'scale',
+    theme: 'ws',
+  });
+
+  tippy('#sociTwitter', {
+    content: 'View my Twitter Profile',
+    arrow: false,
+    animation: 'scale',
+    theme: 'ws',
+  });
+
+  tippy('#sociYoutube', {
+    content: 'View my YouTube Profile',
+    arrow: false,
+    animation: 'scale',
+    theme: 'ws',
+  });
+
+  tippy('#sociMail', {
+    content: 'Mail Me',
+    arrow: false,
+    animation: 'scale',
+    theme: 'ws',
+  });
