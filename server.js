f4378e16b3f7739e30910b768c5e2f76c3473db5
Eleven <incealper00@gmail.com>
2021-07-31 17:17:54 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,23 +1,7 @@
 const express = require("express");
 const app = express();
 app.use(express.static("public"));
-const path = require("path");
 
-  app.engine("html", require("ejs").renderFile);
-  app.set("view engine", "html");
-
-  var bodyParser = require("body-parser");
-  app.use(bodyParser.json());
-
-  const renderTemplate = (res, req, template, data = {}) => {
-    const baseData = {
-      path: req.path
-    };
-    res.render(
-      path.resolve(`${templateDir}${path.sep}${template}`),
-      Object.assign(baseData, data)
-    );
-  };
 
   app.get("/", (req, res) => {
         res.render(res, req, "index");
