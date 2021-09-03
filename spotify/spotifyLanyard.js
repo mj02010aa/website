05b4b53b0874df14cd000fe401919b38017cbc57
Eleven <11@apptime.tech>
2021-09-04 01:20:08 +0300

    Update spotifyLanyard.js

--- spotify/spotifyLanyard.js
+++ spotify/spotifyLanyard.js
@@ -45,7 +45,7 @@ function update_presence() {
     spotifyDetails.innerHTML = `<div class="head-img-section"><img draggable="false" class="spotify-song-img" src="${api.d.spotify.album_art_url}?size=2048"></div><div class="head-text-section"><div class="spotify-text-exp text-gray-100"><br id="head-br"><b>${api.d.spotify.song}</b><br>by ${api.d.spotify.artist}<br>on ${api.d.spotify.album}</div></div>`;
 
   } else {
-    spotifyDetails.innerHTML = `<div class="head-img-section"><img draggable="false" class="spotify-song-img-nob" src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/19/Spotify_logo_without_text.svg/1024px-Spotify_logo_without_text.svg.png?size=2048"></div><div class="head-text-section"><div class="spotify-text-exp text-gray-100"><br id="head-br"><b> </b><br>Not Listening<br> </div></div>`;
+    spotifyDetails.innerHTML = `<div class="head-img-section"><img draggable="false" class="spotify-song-img-gb" src="https://i.scdn.co/image/ab67616d0000b273237665d08de01907e82a7d8a?size=2048?size=2048"></div><div class="head-text-section"><div class="spotify-text-exp text-gray-100"><br id="head-br"><b>Never Gonna Give You Up</b><br>by Rick Astley<br>on Whenever You Need Somebody</div></div>`;
   }
 
 }
