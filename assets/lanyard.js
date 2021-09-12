98c76d2754c2fd135a326c86b6542e50a7e165f3
Eleven <11@apptime.tech>
2021-09-12 14:23:32 +0300

    new

--- assets/lanyard.js
+++ assets/lanyard.js
@@ -42,19 +42,19 @@ function update_presence() {
   }
 
   if (api.d.discord_status === "dnd") {
-    statusContent.innerHTML = `<span class="w-3 h-3 bg-red-500 rounded-full inline-flex ml-1 mr-1"></span> Online`;
+    statusContent.innerHTML = `<span class="w-3 h-3 bg-red-500 rounded-full inline-flex ml-1"></span> Online`;
 
   } else if (api.d.discord_status === "idle") {
-    statusContent.innerHTML = `<span class="w-3 h-3 bg-yellow-500 rounded-full inline-flex ml-1 mr-1"></span> Online`;
+    statusContent.innerHTML = `<span class="w-3 h-3 bg-yellow-500 rounded-full inline-flex ml-1"></span> Online`;
 
   } else if (api.d.discord_status === "online") {
-    statusContent.innerHTML = `<span class="w-3 h-3 bg-green-500 rounded-full inline-flex ml-1 mr-1"></span> Online`;
+    statusContent.innerHTML = `<span class="w-3 h-3 bg-green-500 rounded-full inline-flex ml-1"></span> Online`;
 
   } else if (api.d.discord_status === "offline") {
-    statusContent.innerHTML = `<span class="w-3 h-3 bg-gray-500 rounded-full inline-flex ml-1 mr-1"></span> Offline`;
+    statusContent.innerHTML = `<span class="w-3 h-3 bg-gray-500 rounded-full inline-flex ml-1"></span> Offline`;
 
   } else {
-    statusContent.innerHTML = `<span class="w-3 h-3 bg-gray-500 rounded-full inline-flex ml-1 mr-1"></span> Loading`;
+    statusContent.innerHTML = `<span class="w-3 h-3 bg-gray-500 rounded-full inline-flex ml-1"></span> Loading`;
 
   }
 }
